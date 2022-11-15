3a6271cbda4d58a6125499b725e0f0ca85e3f509
Jrnze <92707592+Jrnze@users.noreply.github.com>
2022-11-15 15:19:01 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
+import { ACTIONS } from './Reducer'
+import DigitButton from './DigitButton';
+import FormatOperand from './FormatOperand';
+import OperationButton from './OperationButton';
+import Reducer from './Reducer';
 import './App.css';
 
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(Reducer, {});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="calculator-grid">
+      <div className='output'>
+        <div className='previous-operand'><FormatOperand operand={previousOperand}/> {operation}</div>
+        <div className='current-operand'><FormatOperand operand={currentOperand}/></div>
+      </div>
+      <button className='span-two' onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
+      <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
+      <OperationButton operation="÷" dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+      <OperationButton operation="x" dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+      <OperationButton operation="+" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+      <OperationButton operation="-" dispatch={dispatch}/>
+      <DigitButton digit="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
+      <button className='span-two' onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>     
+    </div>    
   );
 }
 
---
+++ src/OperationButton.js
@@ -0,0 +1,5 @@
+import { ACTIONS } from './Reducer'
+
+export default function OperationButton({dispatch, operation}) {
+    return <button onClick={() => dispatch({type:ACTIONS.CHOOSE_OPERATION, payload: {operation}})}>{operation}</button>
+}
